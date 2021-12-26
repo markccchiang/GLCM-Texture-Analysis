c44f7404a7364524912941d149bebd1901f678c9
Cheng-Chin Chiang <chengchin.chiang@gmail.com>
2021-12-27 00:20:48 +0800

    add the PolygonController class

---
+++ controller/PolygonController.hpp
@@ -0,0 +1,26 @@
+#ifndef POLYGON_CONTROLLER_HPP_
+#define POLYGON_CONTROLLER_HPP_
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+#include "analysis/TextureAnalysis.hpp"
+#include "viewer/ImageViewer.hpp"
+
+namespace polygon {
+
+class PolygonController {
+public:
+    PolygonController(){};
+    ~PolygonController() = default;
+
+    void Run(const std::string& filename, int d = 1, int Ng = 256);
+
+private:
+    static void MouseCallBackFunc(int event, int x, int y, int flags, void* userdata);
+    static std::vector<std::pair<int, int>> GetMinMax(const std::vector<cv::Point>& vec);
+};
+
+} // namespace polygon
+
+#endif // POLYGON_CONTROLLER_HPP_
--- glcm-analysis.cpp
+++ glcm-analysis.cpp
@@ -1,17 +1,29 @@
 #include "analysis/TextureAnalysis.hpp"
+#include "controller/PolygonController.hpp"
 #include "controller/RectController.hpp"
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        cout << "Usage: ./glcm-analysis <file name>" << endl;
+    if (argc < 2) {
+        cout << "Usage: ./glcm-analysis <file name> <mode: polygon, rectangle>" << endl;
         return 1;
     }
-    string filename = argv[1];
 
-    RectController controller;
-    controller.Run(filename);
+    string filename = argv[1];
+    string mode("rectangle");
+
+    if (argc == 3) {
+        mode = argv[2];
+    }
+
+    if (mode == "polygon") {
+        polygon::PolygonController controller;
+        controller.Run(filename);
+    } else {
+        RectController controller;
+        controller.Run(filename);
+    }
 
     return 0;
 }
